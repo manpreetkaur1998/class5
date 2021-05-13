7d00d479cde993f9e1e844de708eac30d421eee1
unknown <mkaur2563@gmail.com>
2021-05-13 15:38:50 +0530

    c1

---
+++ class8.R
@@ -0,0 +1,35 @@
+#some arithmetic function
+# addition
+3+6
+1+5
+46+98
+# substraction
+7-4
+19-16
+65-43
+7-9
+# square root
+sqrt(16)
+sqrt(78)
+# exponents
+13^6
+13^2
+2^3
+23^2
+# multiplication
+8*2
+4*5
+# division
+100/50
+# mean
+mean(1,10,100) #result should be 37
+mean.default(1,10,100) #result should be 10
+median(1,10,100)
+?median
+?mean
+(c(1,10,100)) #to store our series of values
+mean(c(1,10,100))
+(c(20,40,80))
+mean(c(20,40,80))
+median(c(20,40,80))
+mode(c(20,40,80))
